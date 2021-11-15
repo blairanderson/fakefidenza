0b5b4849efade17a6b71820b79734e3f21a4fa1f
Blair Anderson <blair81@gmail.com>
2021-11-14 22:26:11 -0800

    update title

--- src/Footer.js
+++ src/Footer.js
@@ -3,8 +3,30 @@ import * as React from "react";
 
 function Footer({slowToken, updateHash, changeHash}) {
   return (<nav className="navbar fixed-bottom navbar-light bg-light">
+
+
+    <div className="modal fade in" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div className="modal-body">
+            modal-buttons
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" className="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
     <div className="container-fluid">
       <a className="navbar-brand" href="/">Fauxdenza</a>
+      <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Launch demo modal
+      </button>
       <form className="d-flex" onSubmit={(e) => {
         e.preventDefault();
       }}>
